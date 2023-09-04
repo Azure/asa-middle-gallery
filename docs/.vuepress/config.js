1367255fd459abcddf5a4411b7cd16f7efc8dfa7
hui1110 <hui1110rui@163.com>
2023-09-04 14:08:01 +0800

    add asa button docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,50 @@
+module.exports = {
+    head: [
+        ['link', { rel: 'icon', href: '/assets/Microsoft_Azure.svg' }],
+    ],
+    title: "Azure Spring Apps button Documentation",
+    description: "From code to deployment in minutes",
+    base: "/asa-middle-gallery/",
+    themeConfig: {
+        logo: '/assets/Microsoft_Azure.svg',
+        repo: 'Azure/asa-middle-gallery',
+        docsDir: 'docs',
+        docsBranch: 'main',
+        editLinks: true,
+        lastUpdated: 'Last Updated',
+        sidebarDepth: 1,
+        smoothScroll: true,
+        nav: [
+         { text: "Home", link: '/' },
+         // { text: "Back to NubesGen.com", link: 'https://nubesgen.com' }
+        ],
+      displayAllHeaders: true,
+      sidebar: [
+          {
+              title: '🔹 Azure Spring Apps button',
+              collapsable: false,
+              sidebarDepth: 0,
+              children: [
+                  {
+                      title: 'Overview',
+                      path: '/asa-button/overview',
+                  },
+                  {
+                      title: 'Quick start',
+                      path: '/asa-button/quick-start',
+                  },
+              ],
+          }
+      ]
+    },
+    plugins: [
+      [
+        'vuepress-plugin-clean-urls',
+        {
+          normalSuffix: '/',
+          indexSuffix: '/',
+          notFoundPath: '/404.html',
+        },
+      ],
+    ]
+  };
